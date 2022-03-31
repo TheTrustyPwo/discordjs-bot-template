5a6ebd3b7ff7ffdde6695969b609bdcddd4d5cf6
steeldragon960 <68459550+steeldragon960@users.noreply.github.com>
2022-03-31 21:45:48 +0800

    Add config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    BOT_TOKEN: "",
+    MONGO_CONNECTION: "",
+    ADMIN_IDS: [], // Bot owner ID's
+    INTERACTIONS: {
+        SLASH: true, // Should the interactions be enabled
+        CONTEXT: true, // Should contexts be enabled
+        GLOBAL: false, // Should the interactions be registered globally
+        TEST_GUILD_ID: "", // Guild ID where the interactions should be registered. [** Test you commands here first **]
+    },
+    /* Bot Embed Colors */
+    EMBED_COLORS: {
+        DEFAULT: "#FF8C00",
+        SUCCESS: "#00FF00",
+        ERROR: "#D61A3C",
+        WARNING: "#F7E919",
+    },
+};
