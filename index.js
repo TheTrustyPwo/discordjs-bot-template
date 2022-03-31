2aa9d1a666500968d3b362fc143e99eafacd0c0c
steeldragon960 <68459550+steeldragon960@users.noreply.github.com>
2022-03-31 21:46:17 +0800

    Add index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const { startupCheck } = require("./src/utils/botUtils");
+const BotClient = require("./src/structures/BotClient");
+const config = require("./config");
+
+global.__appRoot = path.resolve(__dirname);
+
+// initialize client
+const client = new BotClient();
+client.loadCommands("src/commands");
+client.loadEvents("src/events");
+
+// find unhandled promise rejections
+process.on("unhandledRejection", (err) => client.logger.error(`Unhandled exception`, err));
+
+(async () => {
+    await startupCheck();
+    await client.initializeMongoose();
+    await client.login(config.BOT_TOKEN);
+})();
